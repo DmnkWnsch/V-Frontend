9ec21fa6a28a781df260728e72d1599dde0bdf83
Moritz-lid <moritz.lidzba@web.de>
2023-11-29 14:34:10 +0100

    follow up zu 'kleine verschönerungen

---
+++ src/routes/exam-administration/custom_components/exam_container.svelte
@@ -0,0 +1,44 @@
+<script>
+// @ts-nocheck
+
+  // import sveltestrap components
+  import {
+      Accordion,
+      AccordionItem,
+      Container,
+      Row,
+      Col,
+      Button,
+      Badge
+      } from 'sveltestrap';
+  
+  // import custom components
+  import Sub_exam_container from './sub_exam_container.svelte';
+
+  export let active;
+  active = ( active == "true" ) ? "active" : "";
+  export let data;
+  
+</script>
+
+<Accordion>
+    <AccordionItem {active}>
+      <h2 slot="header">
+        <div class="d-flex justify-content-between w-100">
+          <b>{data["name"]}</b>
+          {#each data["tags"] as value}
+            <Badge class="mx-1">{value}</Badge>
+          {/each}
+          <span class="mx-5">
+            <b>Leistungspunkte: </b>{data["points"]}
+          </span>
+          <span class="mx-3">
+            <b>ID: </b>{data["id"]}
+          </span>
+        </div>
+      </h2>
+      {#each data["sub_exams"] as value}
+        <svelte:component this={Sub_exam_container} data={value}/>
+      {/each}
+    </AccordionItem>
+  </Accordion>
