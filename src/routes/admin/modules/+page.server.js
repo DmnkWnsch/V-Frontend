b9f7c302658a27536719283040a8ca1b5bb6377c
DmnkWnsch <wuensch_dominik@t-online.de>
2023-12-13 16:34:21 +0100

    Add loading of all modules when loading the page

--- src/routes/admin/modules/+page.server.js
+++ src/routes/admin/modules/+page.server.js
@@ -1,3 +1,18 @@
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ params }) {
+  const allModulesReq = await fetch("http://localhost:3210/modules", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const allModulesRes = await allModulesReq.json();
+
+  return {
+    modules: allModulesRes,
+  };
+}
+
 /** @type {import('./$types').Actions} */
 export const actions = {
   saveModule: async ({ cookies, request }) => {
