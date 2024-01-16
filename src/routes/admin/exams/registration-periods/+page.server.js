ec7cc42791b7e70f773b5ab8061f6acbf4a6bcf6
DmnkWnsch <wuensch_dominik@t-online.de>
2024-01-16 19:06:18 +0100

    Add page to manage registration periods

---
+++ src/routes/admin/exams/registration-periods/+page.server.js
@@ -0,0 +1,64 @@
+// @ts-nocheck
+import consts from "../../../../consts";
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ params }) {
+  const regPeriodsReq = await fetch(consts.API_URL + "/registration-periods", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const regPeriodsRes = await regPeriodsReq.json();
+
+  return {
+    periods: regPeriodsRes,
+  };
+}
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+  editPeriod: async ({ cookies, request }) => {
+    const data = await request.formData();
+    const action = data.get("action");
+    if (action === "save") {
+      savePeriod(data);
+    } else if (action === "delete") {
+      deletePeriod(data);
+    }
+  },
+};
+
+const savePeriod = async (formData) => {
+  const periodId = formData.get("id");
+
+  const periodData = {
+    start_date: formData.get("start_date"),
+    end_date: formData.get("end_date"),
+  };
+
+  const savePeriodReq = await fetch(
+    consts.API_URL + "/registration-periods/" + periodId,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(periodData),
+    }
+  );
+};
+
+const deletePeriod = async (formData) => {
+  const periodId = formData.get("id");
+
+  const deletePeriodReq = await fetch(
+    consts.API_URL + "/registration-periods/" + periodId,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+};
---
+++ src/routes/admin/exams/registration-periods/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  import RegistrationPeriodCard from "../components/RegistrationPeriodCard.svelte";
+
+  export let data;
+</script>
+
+<svelte:head>
+  <title>PV-A Prüfungsverwaltung</title>
+  <meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="container bg-body-tertiary border my-4 p-3 shadow-sm">
+  {#each data.periods as period}
+    <RegistrationPeriodCard
+      id={period.id}
+      periodName={period.name}
+      startDate={period.start_date}
+      endDate={period.end_date}
+    />
+  {/each}
+</div>
