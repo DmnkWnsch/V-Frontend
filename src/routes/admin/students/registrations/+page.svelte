6c0038245f66733458c927f194dc1afcde8fc553
DmnkWnsch <wuensch_dominik@t-online.de>
2024-03-25 17:45:46 +0100

    feat: Start registration management page

--- src/routes/admin/students/registrations/+page.svelte
+++ src/routes/admin/students/registrations/+page.svelte
@@ -7,4 +7,8 @@
   <meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="container bg-light-subtle border my-4 p-3 shadow-sm">Hallo</div>
+<div class="container bg-light-subtle border my-4 p-3 shadow-sm">
+  <form action="?manageRegistration" method="POST" class="row g-3">
+    <div class="col-12 fs-5">Prüfungsanmeldung verwalten</div>
+  </form>
+</div>
