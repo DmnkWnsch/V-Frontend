8cffff831991450df778636e32c45c13e9b3afc8
DmnkWnsch <wuensch_dominik@t-online.de>
2024-03-25 17:38:25 +0100

    feat: Add landing page for admin pages

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,1 +1,19 @@
-Nothing
+<script>
+  import LandingPage from "./components/LandingPage.svelte";
+  import LandingPageItem from "./components/LandingPageItem.svelte";
+</script>
+
+<LandingPage>
+  <LandingPageItem title="Module verwalten" url="/admin/modules"
+    >Module verwalten</LandingPageItem
+  >
+  <LandingPageItem title="Noten verwalten" url="/admin/grades"
+    >Noten verwalten</LandingPageItem
+  >
+  <LandingPageItem title="Prüfungsverwaltung" url="/admin/exams"
+    >Prüfungen verwalten</LandingPageItem
+  >
+  <LandingPageItem title="Studenten verwalten" url="/admin/modules"
+    >Studenten verwalten</LandingPageItem
+  >
+</LandingPage>
