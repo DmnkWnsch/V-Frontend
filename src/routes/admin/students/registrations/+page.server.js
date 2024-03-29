eede3c9cffd8e8ca7924a573789cc51d9cda3ec7
DmnkWnsch <wuensch_dominik@t-online.de>
2024-03-29 18:28:54 +0100

    feat: Add possibility to manage exam registrations

---
+++ src/routes/admin/students/registrations/+page.server.js
@@ -0,0 +1,104 @@
+import consts from "../../../../consts";
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+  addRegistration: async ({ cookies, request }) => {
+    const formData = await request.formData();
+
+    const registration = {
+      member_id: formData.get("id"),
+      exam_plan_id: formData.get("planned_exam"),
+      status: formData.get("status"),
+    };
+
+    const registrationRequest = await fetch(consts.API_URL + "/registrations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(registration),
+    });
+
+    const registrationResponse = await registrationRequest.json();
+    if (registrationRequest.status == 201) {
+      return {
+        success: true,
+        data: registrationResponse.data,
+        reason: "REG_ADD",
+      };
+    } else {
+      return {
+        error: true,
+        message: registrationResponse.message,
+      };
+    }
+  },
+
+  manageRegistration: async ({ cookies, request }) => {
+    const formData = await request.formData();
+    const action = formData.get("action");
+
+    if (action === "delete") {
+      const registration = {
+        member_id: formData.get("id"),
+        exam_plan_id: formData.get("exam_plan_id"),
+      };
+
+      const deleteRegistrationRequest = await fetch(
+        consts.API_URL + "/registrations",
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(registration),
+        }
+      );
+
+      const deleteRegistrationResponse = await deleteRegistrationRequest.json();
+      if (deleteRegistrationRequest.status == 200) {
+        return {
+          success: true,
+          data: deleteRegistrationResponse.data,
+          reason: "REG_DEL",
+        };
+      } else {
+        return {
+          error: true,
+          message: deleteRegistrationResponse.message,
+        };
+      }
+    } else if (action === "change_state") {
+      const registration = {
+        member_id: formData.get("id"),
+        exam_plan_id: formData.get("exam_plan_id"),
+        state: formData.get("new_state"),
+      };
+
+      const updateRegistrationRequest = await fetch(
+        consts.API_URL + "/registrations",
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(registration),
+        }
+      );
+
+      const updateRegistrationResponse = await updateRegistrationRequest.json();
+      if (updateRegistrationRequest.status == 200) {
+        return {
+          success: true,
+          data: updateRegistrationResponse.data,
+          reason: "REG_UPD",
+        };
+      } else {
+        return {
+          error: true,
+          message: updateRegistrationResponse.message,
+        };
+      }
+    }
+  },
+};
--- src/routes/admin/students/registrations/+page.svelte
+++ src/routes/admin/students/registrations/+page.svelte
@@ -1,5 +1,51 @@
 <script>
+  // @ts-nocheck
+
+  import consts from "../../../../consts.js";
+  import MessageBanner from "../../components/MessageBanner.svelte";
+  import AddRegistrationCard from "../components/AddRegistrationCard.svelte";
+  import RegistrationsTable from "../components/RegistrationsTable.svelte";
+
   export let form;
+
+  let searchInput = "";
+  let targetMember = {};
+  let memberFound = false;
+
+  const searchMemberByID = async () => {
+    memberFound = false;
+
+    if (searchInput?.toString().trim().length != 6) {
+      return (targetMember = {});
+    }
+
+    const memberRequest = await fetch(
+      consts.API_URL + "/members/" + searchInput,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (memberRequest.status == 200) {
+      const response = await memberRequest.json();
+
+      if (response.length == 0) {
+        return (targetMember = { error: true });
+      }
+
+      memberFound = true;
+      return (targetMember = response[0]);
+    }
+  };
+
+  let registrationFormOpen = false;
+
+  const changeRegistrationFormState = () => {
+    registrationFormOpen = !registrationFormOpen;
+  };
 </script>
 
 <svelte:head>
@@ -8,7 +54,64 @@
 </svelte:head>
 
 <div class="container bg-light-subtle border my-4 p-3 shadow-sm">
-  <form action="?manageRegistration" method="POST" class="row g-3">
+  {#if form?.success}
+    <MessageBanner type="success">
+      {#if form?.reason == "REG_ADD"}
+        Die Anmeldung wurde erfolgreich gespeichert.
+      {:else if form?.reason == "REG_DEL"}
+        Die Anmeldung wurde erfolgreich gelöscht.
+      {:else if form?.reason == "REG_UPD"}
+        Der neue Status wurde erfolgreich gespeichert.
+      {/if}
+    </MessageBanner>
+  {/if}
+  {#if form?.error}
+    <MessageBanner type="error">
+      {form?.message}
+    </MessageBanner>
+  {/if}
+
+  <div class="row g-3">
     <div class="col-12 fs-5">Prüfungsanmeldung verwalten</div>
-  </form>
+    <div class="col-4">
+      <label class="form-label" for="search">
+        Student durch Matrikelnummer suchen
+      </label>
+      <input
+        type="number"
+        class="form-control"
+        id="search"
+        name="search"
+        placeholder="123456"
+        bind:value={searchInput}
+        on:input={searchMemberByID}
+      />
+    </div>
+    {#if targetMember.name}
+      <div class="col-12 fs-5">
+        Prüfungsanmeldungen von {searchInput}
+      </div>
+      <RegistrationsTable memberId={targetMember.id} />
+
+      <div class="col-12">
+        <button
+          class="btn btn-success"
+          type="button"
+          on:click={changeRegistrationFormState}>Neue Anmeldung</button
+        >
+      </div>
+
+      {#if registrationFormOpen}
+        <div class="col-12">
+          <AddRegistrationCard memberId={targetMember.id} />
+        </div>
+      {/if}
+    {:else if searchInput?.toString().length != 6}
+      <div class="col-12">Bitte eine 6-stellige Matrikelnummer eingeben.</div>
+    {:else if targetMember.error}
+      <div class="col-12">
+        <b>Fehler:</b> Die Matrikelnummer <b>{searchInput}</b> ist nicht vergeben.
+      </div>
+    {/if}
+  </div>
 </div>
