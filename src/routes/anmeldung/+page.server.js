99db9b33ca5e354072005fc466ab4a5a1183ee5e
DmnkWnsch <wuensch_dominik@t-online.de>
2024-04-16 23:19:59 +0200

    feat: Add methods to (un)register

--- src/routes/anmeldung/+page.server.js
+++ src/routes/anmeldung/+page.server.js
@@ -1,13 +1,14 @@
 // @ts-nocheck
 import consts from "../../consts";
+import { storedMemberId } from "../MemberIdStore";
 
 /** @type {import('./$types').PageServerLoad} */
-// @ts-ignore
-export async function load({params}) {   
-        const ModulesReq = await fetch(consts.API_URL + "/modules" , {
-                method: "GET",
-                headers: { "Content-Type": "application/json",},});
-        /*const courseOneModulesReq = await fetch(consts.API_URL + "courses/1/modules" , {
+export async function load({ params }) {
+  const ModulesReq = await fetch(consts.API_URL + "/modules", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+  /*const courseOneModulesReq = await fetch(consts.API_URL + "courses/1/modules" , {
                 method: "GET",
                 headers: { "Content-Type": "application/json",},});                      
         const courseTwoModulesReq = await fetch(consts.API_URL + "courses/2/modules" , {
@@ -19,16 +20,106 @@ export async function load({params}) {
         const courseFourModulesReq = await fetch(consts.API_URL + "courses/4/modules" , {
                 method: "GET",
                 headers: { "Content-Type": "application/json",},});*/
-        const ModulesRes = await ModulesReq.json();
-        /*const courseOneModulesRes = await courseOneModulesReq.json();
+  const ModulesRes = await ModulesReq.json();
+  /*const courseOneModulesRes = await courseOneModulesReq.json();
         const courseTwoModulesRes = await courseTwoModulesReq.json();
         const courseThreeModulesRes = await courseThreeModulesReq.json();
         const courseFourModulesRes = await courseFourModulesReq.json();*/
-        return {modules: ModulesRes,
-                /*courseOneModules: courseOneModulesRes,
+
+  const registrationsRequest = await fetch(
+    consts.API_URL + "/members/" + storedMemberId + "/registrations",
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+
+  const registrationsResponse = await registrationsRequest.json();
+
+  const resultsRequest = await fetch(
+    consts.API_URL + "/members/" + storedMemberId + "/results",
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+
+  const resultsResponse = await resultsRequest.json();
+
+  return {
+    modules: ModulesRes,
+    registrations: registrationsResponse.data,
+    results: resultsResponse,
+    /*courseOneModules: courseOneModulesRes,
                 courseTwoModules: courseTwoModulesRes,
                 courseThreeModules: courseThreeModulesRes,
                 courseFourModules: courseFourModulesRes,*/
-                };
-       
+  };
 }
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+  register: async ({ cookies, request }) => {
+    const formData = await request.formData();
+
+    const registration = {
+      member_id: formData.get("member"),
+      exam_plan_id: formData.get("plan_id"),
+    };
+
+    const registrationRequest = await fetch(consts.API_URL + "/registrations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(registration),
+    });
+
+    const registrationResponse = await registrationRequest.json();
+    if (registrationRequest.status == 201) {
+      return {
+        success: true,
+        data: registrationResponse.data,
+        reason: "REG_SUC",
+      };
+    } else {
+      return {
+        error: true,
+        message: registrationResponse.message,
+      };
+    }
+  },
+  unregister: async ({ cookies, request }) => {
+    const formData = await request.formData();
+
+    const registration = {
+      member_id: formData.get("member"),
+      exam_plan_id: formData.get("plan_id"),
+    };
+
+    const deleteRegistrationRequest = await fetch(
+      consts.API_URL + "/registrations",
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(registration),
+      }
+    );
+
+    const deleteRegistrationResponse = await deleteRegistrationRequest.json();
+    if (deleteRegistrationRequest.status == 200) {
+      return {
+        success: true,
+        data: deleteRegistrationResponse.data,
+        reason: "UNREG_SUC",
+      };
+    } else {
+      return {
+        error: true,
+        message: deleteRegistrationResponse.message,
+      };
+    }
+  },
+};
