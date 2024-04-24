b812c1af6f79b02f537df999c4588510402e2d5e
DmnkWnsch <wuensch_dominik@t-online.de>
2024-04-24 20:46:34 +0200

    feat: Finished project

--- src/routes/admin/components/LandingPage.svelte
+++ src/routes/admin/components/LandingPage.svelte
@@ -1,5 +1,11 @@
 <div class="container-fluid d-flex justify-content-center mt-5">
-  <div class="w-50">
+  <div class="w-50 d-none d-lg-block">
+    <slot />
+  </div>
+  <div class="w-75 d-none d-md-block d-lg-none">
+    <slot />
+  </div>
+  <div class="w-100 d-block d-md-none">
     <slot />
   </div>
 </div>
